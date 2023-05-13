358f595b52ffe470e69efd3e4464c13aae0e6a95
vanilla120707 <131329871+vanilla120707@users.noreply.github.com>
2023-05-13 09:08:13 +0800

    多人版骰子遊戲

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > dice) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
 })
+input.onGesture(Gesture.Shake, function () {
+    dice = randint(1, 6)
+    basic.showNumber(dice)
+    radio.sendNumber(dice)
+})
+let dice = 0
+radio.setGroup(1)
